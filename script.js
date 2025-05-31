0271e763438742bad30fd5a7e7653a7db57c44a6
Mannan Shariff <mannanshariff04@gmail.com>
2025-05-31 23:34:20 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,24 @@
 const form = document.getElementById('contactForm');
 const responseMsg = document.getElementById('responseMsg');
 
-form.addEventListener('submit', function(e) {
+form.addEventListener('submit', function (e) {
     e.preventDefault();
-    
-const name = document.getElementById('name').value.trim();
-const email = document.getElementById('email').value.trim();
-const message = document.getElementById('message').value.trim();
 
-if(name && email && message) {
-    responseMsg.textContent = 'Thank you for your message, ' + name + '!';
-    responseMsg.style.color = 'green';
-    form.reset();
-}else {
-    responseMsg.textContent = 'Please fill out all fields.';
-    responseMsg.style.color = 'red';
-}   
+    const name = document.getElementById('name').value.trim();
+    const email = document.getElementById('email').value.trim();
+    const message = document.getElementById('message').value.trim();
+    const number = document.getElementById('number').value.trim();
+
+    if (name && email && message) {
+        responseMsg.innerHTML = 'Thank you for your message, ' + name + '!<br>' +
+                        'Name: ' + name + '<br>' +
+                        'Email: ' + email + '<br>' +
+                        'Number: ' + number + '<br>' +
+                        'Message: ' + message;
+        responseMsg.style.color = 'green';
+        form.reset();
+    } else {
+        responseMsg.textContent = 'Please fill out all fields.';
+        responseMsg.style.color = 'red';
+    }
 });
